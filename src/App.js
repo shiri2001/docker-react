7a7ca122a3e4aa14463ea1dc0097e5c84a51a096
shiri2001 <shirile01@gmail.com>
2022-07-20 11:29:02 +0300

    samantha

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> meeemee
+          Edit <code>src/App.js</code> whats up samantha
         </p>
         <a
           className="App-link"
